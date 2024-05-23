0ab6ec6045ad2a2828a588f0325d07869103bb0e
송지현 <128377424+thdwlgus25@users.noreply.github.com>
2024-05-24 01:37:42 +0900

    3. Lombok 이해하기
    - 어노테이션
    - builder을 사용하는 이유 -> 순서(위치)가 상관없다, 필요한 생성자만 쓸 수 있다.

--- src/main/java/inhatc/spring/shop/controller/HelloController.java
+++ src/main/java/inhatc/spring/shop/controller/HelloController.java
@@ -1,5 +1,6 @@
 package inhatc.spring.shop.controller;
 
+import inhatc.spring.shop.dto.UserDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,12 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.awt.*;
 
-@Controller
+@RestController
 public class HelloController {
 
 //   옛날 방식? @RequestMapping(value = "/", method = RequestMethod.GET)
     @GetMapping("/")
-    public String hello() {
-        return "hello";
+    public UserDto hello() {
+        UserDto userDto = UserDto.builder().name("김길동").age(10).build();
+        userDto.setAge(20);
+        userDto.setName("홍길동");
+        System.out.println("userDto: " + userDto);
+        return userDto;
     }
 }
---
+++ src/main/java/inhatc/spring/shop/dto/UserDto.java
@@ -0,0 +1,16 @@
+package inhatc.spring.shop.dto;
+
+import lombok.*;
+
+@Setter
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserDto {
+    private int age;
+    private String name;
+
+
+}
