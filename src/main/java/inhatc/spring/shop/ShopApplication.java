d81744c0bcdb603de9ac05ba203f4d403ebfb702
송지현 <128377424+thdwlgus25@users.noreply.github.com>
2024-05-24 00:46:11 +0900

    1. 어플리케이션 실행 준비

--- src/main/java/inhatc/spring/shop/ShopApplication.java
+++ src/main/java/inhatc/spring/shop/ShopApplication.java
@@ -2,8 +2,9 @@ package inhatc.spring.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class ShopApplication {
 
 	public static void main(String[] args) {
