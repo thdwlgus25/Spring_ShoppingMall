e912da27592ae139a54abad690d09f6f9f31c055
송지현 <128377424+thdwlgus25@users.noreply.github.com>
2024-05-25 02:52:22 +0900

    6. Item 엔티티 설계하기

---
+++ src/main/java/inhatc/spring/shop/constant/ItemSellStatus.java
@@ -0,0 +1,7 @@
+package inhatc.spring.shop.constant;
+
+// 열거형 : 상수값을 만드는 것
+// 예시는 월 화 수 목 금 토 일, 빨 주 노 초 파 란 보 같은 범위로 사용자가 선택하는 것
+public enum ItemSellStatus {
+    SELL, SOLD_OUT
+}
---
+++ src/main/java/inhatc/spring/shop/entity/Item.java
@@ -0,0 +1,43 @@
+package inhatc.spring.shop.entity;
+
+import inhatc.spring.shop.constant.ItemSellStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "item_id")
+    private Long id; // 아이디
+
+    @Column(nullable = false, length = 50)
+    private String itemName; // 이름
+
+    private int price; // 가격
+
+    private int stockNumber; // 재고 수량
+
+    @Lob // 큰 자료형을 관리할 수 있음
+    @Column(nullable = false)
+    private String itemDetail; // 상품 상세 설명
+
+    @Enumerated(EnumType.STRING)
+    private ItemSellStatus itemSellStatus; // 상품의 판매 상태
+
+
+    private LocalDateTime registerTime; // 등록 시간
+
+    
+    private LocalDateTime updateTime; // 수정 시간
+
+}
