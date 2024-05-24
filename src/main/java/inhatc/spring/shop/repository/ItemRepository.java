253586714ae7b63f840cbd4fde51c156bca2ea9a
송지현 <128377424+thdwlgus25@users.noreply.github.com>
2024-05-25 04:14:23 +0900

    8. 쿼리 메소드
    findByName 쓰기

--- src/main/java/inhatc/spring/shop/repository/ItemRepository.java
+++ src/main/java/inhatc/spring/shop/repository/ItemRepository.java
@@ -3,5 +3,9 @@ package inhatc.spring.shop.repository;
 import inhatc.spring.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    List<Item> findByitemName(String itemName); // selete * from item where itemName ..
 }
--- src/test/java/inhatc/spring/shop/repository/ItemRepositoryTest.java
+++ src/test/java/inhatc/spring/shop/repository/ItemRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,6 +20,32 @@ class ItemRepositoryTest {
     @Autowired
     private ItemRepository itemRepository;
 
+    public void createItemList() {
+        for (int i = 1; i <= 10; i++) {
+            Item item = Item.builder()
+                    .itemName("테스트 상품" + i)
+                    .price(10000 + i)
+                    .stockNumber(100 + i)
+                    .itemDetail("테스트 상품 상세 설명" + i)
+                    .itemSellStatus(ItemSellStatus.SELL)
+                    .registerTime(LocalDateTime.now())
+                    .updateTime(LocalDateTime.now())
+                    .build(); // 기본 생성자 만든거랑 똑같음
+
+            itemRepository.save(item);
+        }
+    }
+    @Test
+    @DisplayName("상품 이름 검색 테스트")
+    public void findByItemNameTest() {
+        createItemList();
+
+        itemRepository
+                .findByitemName("테스트 상품1").
+                forEach((item) -> {
+                    System.out.println(item);
+                });
+    }
     @Test
     @DisplayName("상품 생성 테스트") // 임의로 주고싶은 내용으로 테스트를 할 수 있음
     public void createItemTest(){
