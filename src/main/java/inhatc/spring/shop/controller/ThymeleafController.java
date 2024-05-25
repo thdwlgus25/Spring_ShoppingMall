0dcbfcf6f4a253dc9b0e3570401a2008f9fea7fd
송지현 <128377424+thdwlgus25@users.noreply.github.com>
2024-05-26 04:20:14 +0900

    16. ParamDto를 이용한 매개변수 전달

--- src/main/java/inhatc/spring/shop/controller/ThymeleafController.java
+++ src/main/java/inhatc/spring/shop/controller/ThymeleafController.java
@@ -1,13 +1,17 @@
 package inhatc.spring.shop.controller;
 
 import inhatc.spring.shop.dto.ItemDto;
+import inhatc.spring.shop.dto.ParamDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.awt.*;
 
 @Controller
+@Slf4j
 public class ThymeleafController {
 
     @GetMapping("/thymeleaf/ex1")
@@ -27,5 +31,12 @@ public class ThymeleafController {
     @GetMapping("/thymeleaf/ex2")
     public String ex2() {
         return "thymeleaf/ex2";
+    }
+
+    @GetMapping("/thymeleaf/ex3")
+    public String ex3(ParamDto paramDto, Model model) {
+        log.info("---> paramDto: " + paramDto );
+        model.addAttribute("dto", paramDto);
+        return "thymeleaf/ex3";
     }
 }
---
+++ src/main/java/inhatc/spring/shop/dto/ParamDto.java
@@ -0,0 +1,15 @@
+package inhatc.spring.shop.dto;
+
+import lombok.*;
+
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ParamDto {
+
+    private String name;
+    private int age;
+}
