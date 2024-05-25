220a11820fb7ce965f555b5d4f29cb5b9b058a3d
송지현 <128377424+thdwlgus25@users.noreply.github.com>
2024-05-26 00:40:39 +0900

    10. querydsl 기초

--- src/test/java/inhatc/spring/shop/repository/ItemRepositoryTest.java
+++ src/test/java/inhatc/spring/shop/repository/ItemRepositoryTest.java
@@ -1,24 +1,33 @@
 package inhatc.spring.shop.repository;
 
+import com.querydsl.core.Tuple;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import inhatc.spring.shop.constant.ItemSellStatus;
 import inhatc.spring.shop.entity.Item;
-import jakarta.transaction.Transactional;
+import inhatc.spring.shop.entity.QItem;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static inhatc.spring.shop.entity.QItem.item;
 
 @SpringBootTest
-@Transactional // 남아있던 데이터를 지움
+//@Transactional // 남아있던 데이터를 지움
 class ItemRepositoryTest {
 
     @Autowired
     private ItemRepository itemRepository;
+    
+    @Autowired
+    private EntityManager em;
 
     public void createItemList() {
         for (int i = 1; i <= 10; i++) {
@@ -34,6 +43,57 @@ class ItemRepositoryTest {
 
             itemRepository.save(item);
         }
+    }
+
+    public void createItemList2() {
+        for (int i = 1; i <= 5; i++) {
+            Item item = Item.builder()
+                    .itemName("테스트 상품" + i)
+                    .price(10000 + i)
+                    .stockNumber(100 + i)
+                    .itemDetail("테스트 상품 상세 설명" + i)
+                    .itemSellStatus(ItemSellStatus.SELL)
+                    .registerTime(LocalDateTime.now())
+                    .updateTime(LocalDateTime.now())
+                    .build(); // 기본 생성자 만든거랑 똑같음
+
+            itemRepository.save(item);
+        }
+        for (int i = 6; i <= 10; i++) {
+            Item item = Item.builder()
+                    .itemName("테스트 상품" + i)
+                    .price(10000 + i)
+                    .stockNumber(100 + i)
+                    .itemDetail("테스트 상품 상세 설명" + i)
+                    .itemSellStatus(ItemSellStatus.SOLD_OUT)
+                    .registerTime(LocalDateTime.now())
+                    .updateTime(LocalDateTime.now())
+                    .build(); // 기본 생성자 만든거랑 똑같음
+
+            itemRepository.save(item);
+        }
+    }
+    @Test
+    @DisplayName("")
+    public void querydslTest2() {
+        createItemList2();
+
+    }
+
+    @Test
+    @DisplayName("querydsl 테스트")
+    public void querydslTest() {
+        createItemList();
+        JPAQueryFactory query = new JPAQueryFactory(em);
+
+        List<Item> itemList = query.selectFrom(item)
+                .where(item.itemSellStatus.eq(ItemSellStatus.SELL))
+                .where(item.itemDetail.like("%" + "1" + "%"))
+                .orderBy(item.price.desc())
+                .fetch();
+
+        itemList.forEach(item -> System.out.println(item));
+
     }
 
     @Test
