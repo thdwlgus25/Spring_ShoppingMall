6994cca6d3a25a998fda194362fadeb719163105
송지현 <128377424+thdwlgus25@users.noreply.github.com>
2024-05-25 21:46:52 +0900

    9. JPQL 기초
    JPQL, Native의 차이
    JPQL은 자바 엔티티를 바라보고 한다면, Native는 데이터베이스를 바라보고 함

--- src/main/java/inhatc/spring/shop/repository/ItemRepository.java
+++ src/main/java/inhatc/spring/shop/repository/ItemRepository.java
@@ -2,10 +2,21 @@ package inhatc.spring.shop.repository;
 
 import inhatc.spring.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
-    List<Item> findByitemName(String itemName); // selete * from item where itemName ..
+    List<Item> findByItemName(String itemName); // selete * from item where itemName ..
+
+    List<Item> findByItemNameOrItemDetail(String itemName, String itemDetail);
+
+    List<Item> findByPriceLessThanOrderByPriceDesc(int price);
+    @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc" )
+    List<Item> findByDetail(@Param("itemDetail") String itemDetail); // JPQL
+
+    @Query(value = "select * from Item i where i.item_Detail like %:itemDetail% order by i.price asc", nativeQuery = true )
+    List<Item> findByDetailNative(@Param("itemDetail") String itemDetail); // Native
 }
--- src/test/java/inhatc/spring/shop/repository/ItemRepositoryTest.java
+++ src/test/java/inhatc/spring/shop/repository/ItemRepositoryTest.java
@@ -35,13 +35,53 @@ class ItemRepositoryTest {
             itemRepository.save(item);
         }
     }
+
+    @Test
+    @DisplayName("JPQL 테스트")
+    public void findByDetailTest() {
+        createItemList();
+        itemRepository.findByDetail("1")
+                .forEach((item) -> {
+                    System.out.println(item);
+                });
+    }
+
+    @Test
+    @DisplayName("Native 테스트")
+    public void findByDetailNativeTest() {
+        createItemList();
+        itemRepository.findByDetailNative("1")
+                .forEach((item) -> {
+                    System.out.println(item);
+                });
+    }
+
+    @Test
+    @DisplayName("OrderBy 테스트")
+    public void findByPriceLessThanOrderByPriceDesc() {
+        createItemList();
+        itemRepository.findByPriceLessThanOrderByPriceDesc(100007)
+                .forEach(System.out::println);
+
+    }
+
+    @Test
+    @DisplayName("OR 테스트")
+    public void findByItemNameOrItemDetailTest() {
+        createItemList();
+        List<Item> itemList = itemRepository.findByItemNameOrItemDetail("테스트 상품2", "테스트 상품 상세 설명8");
+        itemList.forEach((item) -> {
+            System.out.println(item);
+        });
+    }
+
     @Test
     @DisplayName("상품 이름 검색 테스트")
     public void findByItemNameTest() {
         createItemList();
 
         itemRepository
-                .findByitemName("테스트 상품1").
+                .findByItemName("테스트 상품1").
                 forEach((item) -> {
                     System.out.println(item);
                 });
