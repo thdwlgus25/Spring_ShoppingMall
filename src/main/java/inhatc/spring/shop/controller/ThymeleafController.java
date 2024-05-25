ea36c1b74e288a0f02f6fbb951f9ba77ea3a1534
송지현 <128377424+thdwlgus25@users.noreply.github.com>
2024-05-26 02:52:27 +0900

    13. Controller에 있는 데이터를 View로 전달하기

--- src/main/java/inhatc/spring/shop/controller/ThymeleafController.java
+++ src/main/java/inhatc/spring/shop/controller/ThymeleafController.java
@@ -1,14 +1,20 @@
 package inhatc.spring.shop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.awt.*;
 
 @Controller
 public class ThymeleafController {
 
     @GetMapping("/thymeleaf/ex1")
-    public String ex1() {
-        System.out.println("123");
+    public String ex1(Model model) {
+
+        Point p = new Point(10, 20);
+
+        model.addAttribute("data", p);
         return "thymeleaf/ex1";
     }
 }
