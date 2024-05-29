b02a1105a28ffb7d1a33c918039e682c30b5a9df
송지현 <128377424+thdwlgus25@users.noreply.github.com>
2024-05-29 21:27:56 +0900

    32. JPA 연관관계 다대일 단방향 매핑

--- src/main/java/inhatc/spring/shop/entity/Cart.java
+++ src/main/java/inhatc/spring/shop/entity/Cart.java
@@ -18,7 +18,7 @@ public class Cart {
     private Long id; // 아이디
 
     // Member 자체를 가져옴
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY) // 필요할 때만 가져오게 하기 위해서(성능을 위해)
     @JoinColumn(name = "member_id")
     private Member member;
 }
---
+++ src/main/java/inhatc/spring/shop/entity/CartItem.java
@@ -0,0 +1,29 @@
+package inhatc.spring.shop.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cart_item_id")
+    private Long id; // 아이디
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "card_id")
+    private Cart cart;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    private int count;
+}
