dc96348a31d8fe9a20b27641b3b55902cdfce249
송지현 <128377424+thdwlgus25@users.noreply.github.com>
2024-07-04 10:05:12 +0900

    40. 상품 등록 페이지 만들기

---
+++ src/main/java/inhatc/spring/shop/controller/ItemController.java
@@ -0,0 +1,16 @@
+package inhatc.spring.shop.controller;
+
+import inhatc.spring.shop.dto.ItemFormDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ItemController {
+
+    @GetMapping("/admin/item/new")
+    public String itemForm(Model model) {
+        model.addAttribute("itemFormDto", new ItemFormDto());
+        return "item/itemForm";
+    }
+}
