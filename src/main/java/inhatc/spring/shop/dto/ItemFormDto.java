065d78b5a9caca52af93545472f9dc5e185b914b
송지현 <128377424+thdwlgus25@users.noreply.github.com>
2024-07-04 10:17:17 +0900

    41. ItemFormDto 수정

--- src/main/java/inhatc/spring/shop/dto/ItemFormDto.java
+++ src/main/java/inhatc/spring/shop/dto/ItemFormDto.java
@@ -2,6 +2,8 @@ package inhatc.spring.shop.dto;
 
 import inhatc.spring.shop.constant.ItemSellStatus;
 import inhatc.spring.shop.entity.Item;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.modelmapper.ModelMapper;
 
@@ -18,12 +20,16 @@ public class ItemFormDto {
 
     private Long id; // 아이디
 
+    @NotBlank(message = "상품명은 필수 입력 값입니다.")
     private String itemName; // 이름
 
+    @NotNull(message = "가격은 필수 입력 값입니다.")
     private int price; // 가격
 
+    @NotNull(message = "수량은 필수 입력 값입니다.")
     private int stockNumber; // 재고 수량
 
+    @NotBlank(message = "상품 상세 설명은 필수 입력 값입니다.")
     private String itemDetail; // 상품 상세 설명
 
     private ItemSellStatus itemSellStatus; // 상품의 판매 상태
