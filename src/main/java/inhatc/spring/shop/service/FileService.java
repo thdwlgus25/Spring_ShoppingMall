957332631fd0cfd0103d91f15a63857dfb7d99c9
송지현 <128377424+thdwlgus25@users.noreply.github.com>
2024-07-06 22:48:21 +0900

    43. 파일 삭제하기

--- src/main/java/inhatc/spring/shop/service/FileService.java
+++ src/main/java/inhatc/spring/shop/service/FileService.java
@@ -1,13 +1,16 @@
 package inhatc.spring.shop.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.UUID;
 
 @Service
+@Slf4j
 public class FileService {
 
     public String uploadFile(String uploadPath, String originalFileName, byte[] fileData) throws IOException {
@@ -21,5 +24,16 @@ public class FileService {
         fos.close();
 
         return savedFileName;
+    }
+
+    public void deleteFile(String filePath) {
+        File deleteFile = new File(filePath);
+
+        if(deleteFile.exists()) {
+            deleteFile.delete();
+            log.info("파일을 삭제 했습니다.");
+        } else {
+            log.info("파일이 존재하지 않습니다.");
+        }
     }
 }
