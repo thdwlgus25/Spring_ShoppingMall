8c7d94642b5c9cc612670d8c2753e6d2a2cb56cc
송지현 <128377424+thdwlgus25@users.noreply.github.com>
2024-07-11 22:25:51 +0900

    44. 파일 저장하기

---
+++ src/main/java/inhatc/spring/shop/repository/ItemImgRepository.java
@@ -0,0 +1,7 @@
+package inhatc.spring.shop.repository;
+
+import inhatc.spring.shop.entity.ItemImg;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
+}
